6ad812daf188f75f1656e0cec76074eb1a0e3fb2
tatouroberto <tatouroberto@gmail.com>
2023-09-01 02:50:00 +0200

    add the scr folder with the index.js and dataArray files

---
+++ src/dataArray.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    description: 'Study algorithmes and data structure',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'rest for 30 minutes',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Study JavaScript',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Workout',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'practice javascript',
+    completed: false,
+    index: 4,
+  },
+];
+
+export default { data };
